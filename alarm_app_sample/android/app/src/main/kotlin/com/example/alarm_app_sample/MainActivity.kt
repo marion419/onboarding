b0461ee88c47880920503eb212c76daa1a04e26c
marion419 <daynvi@gmail.com>
2024-10-22 16:57:56 +0900

    Revert "3주차 알람앱까지"
    This reverts commit a0ac12cecd02a62c84febe62ef3947d2d5571ca9.

--- alarm_app_sample/android/app/src/main/kotlin/com/example/alarm_app_sample/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.alarm_app_sample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- statefulwidget_setstate_sample/android/app/src/main/kotlin/com/example/statefulwidget_setstate_sample/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.statefulwidget_setstate_sample
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
